e58d81a35e654c6269ca2527c064906df9a4143a
Kang-min Liu <gugod@gugod.org>
2018-08-12 21:31:15 +0900

    reformat

--- 00.pl
+++ 00.pl
@@ -2,11 +2,12 @@
 
 Can (a== 1 && a ==2 && a==3) ever evaluate to true?
 
-Is it ever possible that (a== 1 && a ==2 && a==3) could evaluate to true in JavaScript?
+Is it ever possible that (a== 1 && a ==2 && a==3) could evaluate to
+true in JavaScript?
 
-This is an interview question asked by a major tech company. It happened two weeks back, but I'm
-still trying to find the answer. I know we never write such code in our day-to-day job, but I'm
-curious.
+This is an interview question asked by a major tech company. It
+happened two weeks back, but I'm still trying to find the answer. I
+know we never write such code in our day-to-day job, but I'm curious.
 
 https://stackoverflow.com/questions/48270127/can-a-1-a-2-a-3-ever-evaluate-to-true
 
