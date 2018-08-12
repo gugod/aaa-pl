55fc47057d71201181a7b2399b33ef72e14685dc
Kang-min Liu <gugod@gugod.org>
2018-08-12 21:56:30 +0900

    simplify

--- 01.pl
+++ 01.pl
@@ -1,11 +1,11 @@
 use strict;
 
 package ObjA {
-    use overload '==' => sub { $_[0][0]++; };
-    sub new { bless [$_[1]], $_[0] }
+    use overload '==' => sub { 1 };
+    sub new { bless {}; }
 };
 
-my $a = ObjA->new(1);
+my $a = ObjA->new();
 
 if ($a == 1 && $a == 2 && $a == 3) {
     print "Just A Perl Hacker\n";
