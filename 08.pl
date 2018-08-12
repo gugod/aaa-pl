ce6519327ef96f299df854778120eee4b34b07b3
Kang-min Liu <gugod@gugod.org>
2018-08-13 05:38:07 +0900

    reorder

--- 08.pl
+++ 08.pl
@@ -1,6 +1,7 @@
-local $/; open 0; eval(<0> =~ s{==}{=}gr =~ s{\A(.+\}sr\);)}{}sr);
+print "Just A Perl Hacker\n";
+__END__
 
-my $a;
 if ($a==1 && $a==2 && $a==3) {
     print "Just A Perl Hacker\n";
 }
+
