9700d422834009215b9648e86c42d5647895e7e7
Kang-min Liu <gugod@gugod.org>
2018-08-15 14:12:46 +0100

    indent

--- slide.sh
+++ slide.sh
@@ -1,2 +1,12 @@
 #/bin/bash
-for f in 0*.pl; do ( clear; echo '# '$f; cat $f; read ; echo "#=> " ; perl $f ; read); done
+for f in 0*.pl; do
+    clear;
+    echo '# '$f
+    cat $f
+    # source-highlight --out-format esc --src-lang perl < $f
+    read
+    echo "# perl $f"
+    echo "#=> "
+    perl $f
+    read
+done
